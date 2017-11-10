e05325429de77f5e9dff7ec8939715124f4d34aa
nqh-ictu <nqh-ictu@gmail.com>
2017-11-10 10:05:45 +0700

    Commit Test

--- app/src/main/java/com/example/nqh/demoapp/MainActivity.java
+++ app/src/main/java/com/example/nqh/demoapp/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int main;
-        void main;
-        int a;
+        float b;
     }
 }
